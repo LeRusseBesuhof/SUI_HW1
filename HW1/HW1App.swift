acf31f68d4e55baae996ae529c9da9a8311293fa
LeRusseBesuhof <p.grad0ff@gmail.com>
2024-07-18 18:03:55 +0700

    Initial Commit

---
+++ HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by Павел Градов on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
